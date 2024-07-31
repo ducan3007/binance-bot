10f4847ffd3c83b72dffb531b14c04d5bff72eb9
ducan3007 <lordmasterking001@gmail.com>
2024-07-31 22:02:00 +0700

    d

--- binance_24hr_tickers_v2.py
+++ binance_24hr_tickers_v2.py
@@ -3,8 +3,6 @@ import time
 from tabulate import tabulate
 from logger import logger
 
-N = 50
-
 
 def format_table(data):
     gainers = data["gainers"]
@@ -29,72 +27,40 @@ def format_table(data):
     return tabulate(table_data, headers=["GAINERS", "24h%", "LOSERS", "24h%"], tablefmt="simple")
 
 
-def get_24h_price_change():
-    url = "https://fapi.binance.com/fapi/v1/ticker/24hr"
+def get_24h_price_change(symbol):
+    url = f"https://fapi.binance.com/fapi/v1/ticker/24hr?symbol={symbol}"
     response = requests.get(url)
     data = response.json()
     return data
 
 
-def check_symbol_trade_status(symbol):
-    url = f"https://fapi.binance.com/fapi/v1/openInterest?symbol={symbol}"
-    response = requests.get(url)
-    if response.status_code == 200:
-        return True
-    return False
+def get_top_gainers_and_losers(tokens):
+    data = []
+    for symbol in tokens:
+        try:
+            ticker_data = get_24h_price_change(symbol)
+            data.append(ticker_data)
+        except Exception as e:
+            logger.error(f"Error fetching data for {symbol}: {e}")
 
-
-def filter_usdt_pairs(data):
-    usdt_pairs = [ticker for ticker in data if ticker["symbol"].endswith("USDT")]
-    return usdt_pairs
-
-
-def get_top_gainers_and_losers(data):
+    print(data)
     sorted_data = sorted(data, key=lambda x: float(x["priceChangePercent"]), reverse=True)
 
-    top_gainers = []
-    top_losers = []
+    top_gainers = [item for item in sorted_data if float(item["priceChangePercent"]) > 0][:50]
 
-    gainers_checked = set()
-    losers_checked = set()
+    sorted_data = sorted(data, key=lambda x: float(x["priceChangePercent"]), reverse=False)
+    top_losers = [item for item in sorted_data if float(item["priceChangePercent"]) < 0][-50:]
 
-    gainers_idx = 0
-    losers_idx = 0
-    counter = 0
-    # Collect top gainers
-    while len(top_gainers) < N and gainers_idx < len(sorted_data):
-        candidate = sorted_data[gainers_idx]
-        if candidate["symbol"] not in gainers_checked:
-            gainers_checked.add(candidate["symbol"])
-            if check_symbol_trade_status(candidate["symbol"]):
-                counter += 1
-                if float(candidate["priceChangePercent"]) < 1.0:
-                    break
-                top_gainers.append(candidate)
-        gainers_idx += 1
-
-    # Collect top losers
-    sorted_losers_data = sorted(data, key=lambda x: float(x["priceChangePercent"]))
-    while len(top_losers) < N and losers_idx < len(sorted_losers_data):
-        candidate = sorted_losers_data[losers_idx]
-        if candidate["symbol"] not in losers_checked:
-            losers_checked.add(candidate["symbol"])
-            if check_symbol_trade_status(candidate["symbol"]):
-                counter += 1
-                if float(candidate["priceChangePercent"]) > -1.0:
-                    break
-                top_losers.append(candidate)
-        losers_idx += 1
-
-    logger.info(f"Total pairs checked: {counter}")
     return top_gainers, top_losers
 
 
 def main():
     start = time.time()
-    data = get_24h_price_change()
-    usdt_pairs = filter_usdt_pairs(data)
-    top_gainers, top_losers = get_top_gainers_and_losers(usdt_pairs)
+
+    with open(f"tokens.15m.normal.txt", "r") as file:
+        tokens = [f"{line.strip()}USDT" for line in file]
+
+    top_gainers, top_losers = get_top_gainers_and_losers(tokens)
 
     result = {
         "gainers": [
@@ -116,12 +82,12 @@ if __name__ == "__main__":
     table = format_table(message)
     date = time.strftime("%Y-%m-%d", time.localtime())
     message = (
-        f"#DAILY_REPORT {date}\n\nBinance Future Top 50 Gainers & Losers 📊\n\n<pre language='javascript'>{table}</pre>"
+        f"#DAILY_REPORT {date}\n\n<pre language='javascript'>{table}</pre>"
     )
     URL = "http://localhost:8000/send24hrPriceChange"
     response = requests.post(
         URL,
-        json={"message": message, "time_frame": "2h"},
+        json={"message": message, "time_frame": "15m_normal"},
         headers={"Content-Type": "application/json"},
     )
     print(response.json())
