993a72eb062219e26bd046e8b950f241cf9b2f46
ducan3007 <lordmasterking001@gmail.com>
2024-11-15 11:09:05 +0700

    d

--- chandelier_exit.py
+++ chandelier_exit.py
@@ -350,7 +350,7 @@ def main(data, TOKEN, TIME_FRAME, PAIR, TIME_SLEEP, MODE, EXCHANGE):
             )
             break
 
-        if TOKEN not in ["BTC"] and (TIME_FRAME == "5m") or (TIME_FRAME == "15m"):
+        if TOKEN not in ["BTC"] and ((TIME_FRAME == "5m") or (TIME_FRAME == "15m")):
             """
             Pre send telegram message before candle close
             """
