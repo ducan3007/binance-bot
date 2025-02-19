64122c94612bc07e5a1504b7fd2b62b28e808302
ducan3007 <lordmasterking001@gmail.com>
2025-02-19 11:41:40 +0700

    d

--- main.py
+++ main.py
@@ -43,7 +43,7 @@ BOT = {
     "3m": {"chat_id": CHAT_ID_TOP, "token": TOKEN_TOP},
     "5m": {"chat_id": CHAT_ID_15M_V2, "token": TOKEN_15M_V2},
     "15m_v2": {"chat_id": CHAT_ID_15M_V2, "token": TOKEN_15M_V2},
-    "15m": {"chat_id": CHAT_ID_15M_V2, "token": TOKEN_15M_V2},
+    "15m": {"chat_id": CHAT_ID_15M, "token": TOKEN_15M},
     "30m": {"chat_id": CHAT_ID_15M_V2, "token": TOKEN_15M_V2},
     "30m_normal": {"chat_id": CHAT_ID_15M, "token": TOKEN_15M},
     "15m_normal":  {"chat_id": CHAT_ID_15M, "token": TOKEN_15M},
