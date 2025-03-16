ad55881298261bac364657955614ab40d17f4d52
ducan3007 <lordmasterking001@gmail.com>
2025-03-16 16:55:35 +0700

    d

--- chart_test.py
+++ chart_test.py
@@ -149,9 +149,9 @@ def concatenate_images(image1_path, image2_path, output_path):
 
 
 PARI_MAP = {
-    "5m": [{"tf": "5m", "view": 100, "mode": "heikin_ashi"}, {"tf": "15m", "view": 65, "mode": "kline"}],
-    "15m": [{"tf": "15m", "view": 72, "mode": "heikin_ashi"}, {"tf": "30m", "view": 84, "mode": "kline"}],
-    "1h": [{"tf": "1h", "view": 48, "mode": "heikin_ashi"}, {"tf": "4h", "view": 42, "mode": "kline"}],
+    "5m": [{"tf": "5m", "view": 96, "mode": "heikin_ashi"}, {"tf": "15m", "view": 48, "mode": "kline"}],
+    "15m": [{"tf": "15m", "view": 72, "mode": "heikin_ashi"}, {"tf": "30m", "view": 60, "mode": "kline"}],
+    "1h": [{"tf": "1h", "view": 48, "mode": "heikin_ashi"}, {"tf": "4h", "view": 30, "mode": "kline"}],
 }
 
 
